af5a54e8f119f05fe7551b9c3cc7f4f8b8eb613d
akashpr4deep <akashpradeep.aa@gmail.com>
2025-11-23 15:11:01 +0530

    source-code.py

--- source-code.py
+++ source-code.py
@@ -6,8 +6,7 @@ import random
 
 choice='y'
 while choice=='y':
-  #Entering the values from the user
-  user_input=input("Enter the area you want an energy-saving tip for (lighting,heating,kitchen,electronics,general): ")
+  user_input=input("Enter the area you want an energy saving tip for (lighting,heating,kitchen,electronics,general): ")
   if user_input.lower()=='lighting':
      c='y'
      while c=='y':
@@ -22,10 +21,10 @@ while choice=='y':
   elif user_input.lower()=='heating':
        c='y'
        while c=='y':
-            tips=["Lower your thermostat by a few degrees (e.g., 2°C) when you're asleep or away to **reduce HVAC electricity use**.",
-        "Seal air leaks around windows and doors; this reduces the amount of time your **electric heater or AC runs**.",
-        "Clean or replace your furnace and AC filters monthly; a dirty filter forces the **HVAC fan motor to work harder**.",
-        "If using central air, close vents in unused rooms to **efficiently direct cooled air**."]
+            tips=["Lower your thermostat by a few degrees (e.g., 2°C) when you're asleep or away to **reduce HVAC electricity use.",
+        "Seal air leaks around windows and doors; this reduces the amount of time your **electric heater or AC runs.",
+        "Clean or replace your furnace and AC filters monthly; a dirty filter forces the **HVAC fan motor to work harder.",
+        "If using central air, close vents in unused rooms to **efficiently direct cooled air."]
             r=random.randint(0,3)
             print(tips[r])
             c=input('Do you wish to see more tips(y/n): ')
@@ -37,7 +36,7 @@ while choice=='y':
           tips=["Use the **microwave or toaster oven** instead of the main oven when possible (they consume less electricity).",
         "Keep your refrigerator and freezer settings optimal: **37°F (3°C) for the fridge and 0°F (-18°C)** for the freezer.",
         "Run your dishwasher only when it's full and use the **air-dry setting** instead of the heated dry cycle.",
-         "Defrost your freezer regularly, as **ice buildup forces the motor to use more electricity**."]
+         "Defrost your freezer regularly, as **ice buildup forces the motor to use more electricity."]
           r=random.randint(0,3)
           print(tips[r])
           c=input('Do you wish to see more tips(y/n): ')
@@ -75,3 +74,4 @@ while choice=='y':
     print("---------------------------------------------------------------")
     print("Thank You")
     print("---------------------------------------------------------------")
+
